ba70a4905530db0d07d1d74aed2f27bb81db9155
Anthony Ioimo <anthonylioimo@gmail.com>
2019-01-30 10:01:50 +0100

    add isChainValid method

--- main.js
+++ main.js
@@ -32,6 +32,16 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
   }
+
+  isChainValid() {
+    for (let i = 1; i<this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i-1]
+      if(currentBlock.hash !== currentBlock.calculateHash()) return false
+      if (currentBlock.previousHash !== previousBlock.hash) return false
+    }
+    return true
+  }
 }
 
 let myCoin = new Blockchain();
@@ -39,4 +49,10 @@ myCoin.addBlock(new Block(1, "05/01/2019", { amount: 20 }));
 myCoin.addBlock(new Block(2, "10/01/2019", { amount: 105 }));
 myCoin.addBlock(new Block(3, "15/01/2019", { amount: 3 }));
 
-console.log(JSON.stringify(myCoin, null, 2))
+console.log("Blockchain validity: ", myCoin.isChainValid())
+
+console.log("Attempted tampering of Value and recalcuating Hash")
+myCoin.chain[1].data.amount = 10000
+myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
+
+console.log("Blockchain validity: ", myCoin.isChainValid())
